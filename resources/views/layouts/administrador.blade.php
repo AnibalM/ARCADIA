c3994c4e3aab9f8747d0cb2ebc7aeeffa0ac6358
AnibalM <anjomalo17@gmail.com>
2019-01-10 11:26:06 -0500

    INDEPENDICE EL PRINCIPAL DE INTERNET

--- resources/views/layouts/administrador.blade.php
+++ resources/views/layouts/administrador.blade.php
@@ -8,9 +8,9 @@
     <title>Arcadia</title>
     <meta name="description" content="A high-quality &amp; free Bootstrap admin dashboard template pack that comes with lots of templates and components.">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="{{ asset('administradores/all.css') }}" rel="stylesheet">
+    <link href="{{ asset('administradores/icon.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('administradores/bootstrap.min.css') }}" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="{{ asset('administradores/styles/shards-dashboards.1.1.0.min.css') }}">
     <link rel="stylesheet" href="{{ asset('administradores/styles/extras.1.1.0.min.css') }}">
     @yield('style')
@@ -186,14 +186,14 @@
     <script src="{{ asset('js/popper.min.js') }}"></script>
     <!--<script src="{{ asset('js/bootstrap.min.js') }}"></script>-->
     <script src="{{ asset('js/jquery-3.3.1.js') }}"></script>   
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <script async defer src="{{ asset('administradores/scripts/buttons.js') }}"></script>
     
     <script type="text/javascript" src="{{ asset('js/notify.js') }}"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
-    <script src="https://unpkg.com/shards-ui@latest/dist/js/shards.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Sharrre/2.0.1/jquery.sharrre.min.js"></script>
+    <script src="{{ asset('administradores/scripts/popper.min.js') }}" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="{{ asset('administradores/scripts/bootstrap.min.js') }}" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="{{ asset('administradores/scripts/Chart.min.js') }}"></script>
+    <script src="{{ asset('administradores/scripts/shards.min.js') }}"></script>
+    <script src="{{ asset('administradores/scripts/jquery.sharrre.min.js') }}"></script>
     <script src="{{ asset('administradores/scripts/extras.1.1.0.min.js') }}"></script>
     <script src="{{ asset('administradores/scripts/shards-dashboards.1.1.0.min.js') }}"></script>
     <script src="{{ asset('administradores/scripts/app/app-blog-overview.1.1.0.js') }}"></script>	
